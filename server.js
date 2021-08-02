82c8128f61f5e0fafe1e3206a327c116cfc39512
Bernadette McKnight <sissyhanks@yahoo.com>
2021-08-02 15:00:22 -0400

    innitial get route created

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/blog-routes'));
 
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
