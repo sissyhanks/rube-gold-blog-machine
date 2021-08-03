f2949b896d773ac9798cd7d034e4c8d461891fa0
Bernadette McKnight <sissyhanks@yahoo.com>
2021-08-03 00:47:11 -0400

    added login and signup views and routes

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -4,4 +4,26 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/profile');
+//     return;
+//   }
+
+  res.render('log-in');
+});
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/profile');
+//     return;
+//   }
+
+  res.render('signup');
+});
+
+
+
 module.exports = router;
