31162298defb76d0167cbd8ae96d7ae980beb8f5
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-13 10:44:12 -0500

    create post and comment models

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { response } = require('express');
+const { Post } = require('../../models');
+
+router.post('/', async(req, res) => {
+  try{
+
+  } catch (err) {
+
+  }
+});
+
+module.exports = router;
---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes} = require('sequelize');
+
+class Comment extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment', 
+});
+
+module.exports = Comment;
