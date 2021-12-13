9a6592d42efd0f4f96773181502f400abe35bd45
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-13 11:01:38 -0500

    create new post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,9 +4,14 @@ const { Post } = require('../../models');
 // .../api/posts
 router.post('/', async(req, res) => {
   try{
-
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    
+    res.status(200).json(newProject);
   } catch (err) {
-
+    res.status(400).json(err);
   }
 });
 
