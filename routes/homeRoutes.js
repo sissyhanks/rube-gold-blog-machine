e05dd5c9a7076b3b828caae91130852561c7f446
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-29 13:38:32 -0500

    update ho,e routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,8 +2,32 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, BlogEntry, Comment } = require('../models');
 
-router.get('/', (req, res) => {
-  res.send('hello tall');
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await BlogEntry.findAll({
+      attributes: [
+      'id',
+      'title',
+      'entry_text'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['user_name']
+      }
+    ]
+    });
+
+    // Pass serialized data and session flag into template
+    console.log(projectData);
+    res.json(projectData);
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 module.exports = router;
