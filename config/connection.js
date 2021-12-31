11e222b7118ab72b3d229193645b0dad89a26cd0
Bernadette McKnight <sissyhanks@yahoo.com>
2021-12-31 15:58:54 -0500

    comment to JAWS db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
 //declare sequelize with let because the database will either be JAWS if in production or MYsql via workbench credentials which are stored in .env file
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_COBALT_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_COBALT_URL);
 } else {
   sequelize = new Sequelize(
     // Database name
@@ -14,9 +14,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     // Password
     process.env.DB_PW,
-    { 
-      host: 'localhost',
-      dialect: 'mysql',
+    {
+      host: "localhost",
+      dialect: "mysql",
       port: 3306,
     }
   );
