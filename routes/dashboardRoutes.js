2487a97d5cb488128d3cd71f2e9fc694cafc989f
Bernadette McKnight <sissyhanks@yahoo.com>
2022-03-24 09:39:25 -0400

    with auth rendering

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,8 +3,11 @@ const sequelize = require("../config/connection");
 const { User, BlogEntry, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   res.render("dashboard", {
+    logged_in: true,
+    login: false,
+    signup: false,
     blogroll: false,
     dashboard: true,
   });
